9d3e2a52935ff5863a981613826e2f64fdf2da1f
grodmir <dionislobacevich@gmail.com>
2024-11-23 16:10:53 +0300

    Added clearing of old log file contents. Added logging of the list of files that need to be migrated

--- src/main/java/org/example/MigrationTool.java
+++ src/main/java/org/example/MigrationTool.java
@@ -9,27 +9,20 @@ import java.util.List;
 public class MigrationTool {
     public static void run() {
         try {
-            // Проверяет (если нет, создаёт) таблицу для хранения выполненных миграций
             MigrationTableManager.ensureMigrationTableExists();
 
-            // Создаём менеджер и передаём в него экземпляр класса для работы с файлами миграций
             MigrationManager migrationManager = new MigrationManager(new MigrationFileReader());
 
-            // Получаем список миграций, которые необходимо выполнить
             List<MigrationFile> migrations = migrationManager.getPendingMigrations();
 
-            // Обрабатываем случай, если список миграций оказался null или пустым
             if (migrations == null || migrations.isEmpty()) {
-                log.info("Нет новых миграций для применения.");
+                log.warn("Нет новых миграций для применения.");
             } else {
                 log.info("Применение миграций:");
-                // Выводим по порядку все миграции, которые должны быть выполнены
-                migrations.forEach(m -> System.out.println(" - " + m.getFileName()));
+                migrations.forEach(m -> log.info(" - {}", m.getFileName()));
 
-                // Создаём экземпляр класса "исполнителя" миграций
                 MigrationExecutor migrationExecutor = new MigrationExecutor();
 
-                // Выполняем все необходимые миграции
                 try {
                     migrationExecutor.executeMigrations(migrations);
                     log.info("Все миграции успешно применены.");
@@ -42,7 +35,7 @@ public class MigrationTool {
         } catch (IOException e) {
             log.error("Ошибка во время настройки миграции: {}", e.getMessage(), e);
         } catch (Exception e) {
-            log.error("Произошла непредвиденная ошибка: {}", e.getMessage(), e);;
+            log.error("Произошла непредвиденная ошибка: {}", e.getMessage(), e);
         }
     }
 }
